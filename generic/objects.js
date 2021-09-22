56ea49a5a4557478655e564208a0f75d630d59b0
Oglop <stefgust@gmail.com>
2021-09-22 21:09:36 +0200

    add traits objects

--- generic/objects.js
+++ generic/objects.js
@@ -57,5 +57,70 @@ module.exports = {
         influence: 0,
         members: [],
         dwellingId: undefined
+    },
+    traits: {
+        human = {
+            str: 0,
+            agi: 0,
+            vit: 0,
+            int: 0,
+            wis: 0,
+            luc: 0,
+            cha: 0
+        },
+        halfElf = {
+            str: 0,
+            agi: 1,
+            vit: -1,
+            int: 1,
+            wis: 0,
+            luc: 0,
+            cha: 1
+        },
+        woodElf = {
+            str: -1,
+            agi: 2,
+            vit: -1,
+            int: 1,
+            wis: 1,
+            luc: 0,
+            cha: 0
+        },
+        darkElf = {
+            str: 0,
+            agi: 2,
+            vit: -1,
+            int: 2,
+            wis: 0,
+            luc: -1,
+            cha: -2
+        },
+        highElf = {
+            str: -1,
+            agi: 1,
+            vit: -1,
+            int: 3,
+            wis: 2,
+            luc: 0,
+            cha: 1
+        },
+        dwarf = {
+            str: 2,
+            agi: 0,
+            vit: 1,
+            int: -1,
+            wis: 1,
+            luc: -2,
+            cha: -1
+        },
+        halfling = {
+            str: -2,
+            agi: 3,
+            vit: -2,
+            int: 0,
+            wis: 0,
+            luc: 4,
+            cha: 2
+        }
     }
 }
