aabeb4569dcef1980dcc5a3ee7867816c67d0384
Oglop <stefgust@gmail.com>
2021-12-27 17:51:06 +0100

    item generation

--- __tests__/enemy.unit.test.js
+++ __tests__/enemy.unit.test.js
@@ -1,6 +1,11 @@
-const { ENUM_ENEMY_ATTACK } = require('../generic/enums')
+const { 
+    ENUM_ENEMY_ATTACK, 
+    ENUM_ENEMY_TYPE,
+    ENUM_ENEMY_STRENGTH
+} = require('../generic/enums')
 const enemyAttackBuilder = require('../build/enemyAttack')
 const { getRandomNumberInRange } = require('../lib/utils')
+const b = require('../build/monster')
 
 describe('enemy attack tests', () => {
     test('get hit attack', () =>  {
@@ -9,5 +14,12 @@ describe('enemy attack tests', () => {
         expect(a.min).toBe(2)
         expect(value).toBeGreaterThanOrEqual(a.min)
         expect(value).toBeLessThanOrEqual(a.max)
+    })
+    test('build should return a vile monster', () => {
+        const actual = b.build({
+            type: ENUM_ENEMY_TYPE.VILE,
+            strength: ENUM_ENEMY_STRENGTH.WEAK
+        })
+        expect(actual.hp).toBeGreaterThan(0)
     })
 })
