5f29e9cf2beb4059c3c53abe7af949e02c400815
Oglop <stefgust@gmail.com>
2022-07-30 21:35:10 +0200

    Work on encounters

---
+++ models/encounter.js
@@ -0,0 +1,30 @@
+const {
+    ENUM_ENCOUNTER_RANGE
+} = require('../generic/enums')
+
+const advanceRange = encounter => {
+
+}
+
+const retreatRange = encounter => {
+
+}
+
+const route = encounter => {
+
+}
+
+const setInitiativeOrder = encounter => {
+
+}
+
+const updateEncounterValues = encounter => {}
+
+
+
+module.exports = {
+    advanceRange, 
+    retreatRange,
+    escape
+}
+
