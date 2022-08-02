c48c15f4a040b459f24ed3a2fe3975f2ad2797ce
Oglop <stefgust@gmail.com>
2022-08-02 21:57:18 +0200

    Add encounter model tests

--- models/encounter.js
+++ models/encounter.js
@@ -3,11 +3,19 @@ const {
 } = require('../generic/enums')
 
 const advanceRange = encounter => {
-
+    switch (encounter.range) {
+        case ENUM_ENCOUNTER_RANGE.FAR: encounter.range = ENUM_ENCOUNTER_RANGE.LONG; break;
+        case ENUM_ENCOUNTER_RANGE.LONG: encounter.range = ENUM_ENCOUNTER_RANGE.SHORT; break;
+        case ENUM_ENCOUNTER_RANGE.SHORT: encounter.range = ENUM_ENCOUNTER_RANGE.SHORT; break;
+    }
 }
 
 const retreatRange = encounter => {
-
+    switch (encounter.range) {
+        case ENUM_ENCOUNTER_RANGE.FAR: encounter.range = ENUM_ENCOUNTER_RANGE.FAR; break;
+        case ENUM_ENCOUNTER_RANGE.LONG: encounter.range = ENUM_ENCOUNTER_RANGE.FAR; break;
+        case ENUM_ENCOUNTER_RANGE.SHORT: encounter.range = ENUM_ENCOUNTER_RANGE.LONG; break;
+    }
 }
 
 const route = encounter => {
