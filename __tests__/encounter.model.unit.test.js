f9f02841d1b83b68d5b1ff36b166ee408ead6f15
Oglop <stefgust@gmail.com>
2022-10-01 21:50:42 +0200

    Add insertInitaitveSort and tests

--- __tests__/encounter.model.unit.test.js
+++ __tests__/encounter.model.unit.test.js
@@ -1,6 +1,7 @@
 const {
     advanceRange,
-    retreatRange
+    retreatRange,
+    insertInitativeSort
 } = require('../models/encounter')
 const {
     ENUM_ENCOUNTER_RANGE
@@ -47,5 +48,35 @@ describe('encounter.model', () => {
         }
         retreatRange(encounter)
         expect(encounter.range).toBe(ENUM_ENCOUNTER_RANGE.FAR)
+    })
+    test('should add item to queue', () => {
+        const queue = []
+        const item = { id: '123', initative: 10 }
+        insertInitativeSort(queue, item)
+        expect(queue.length).toBe(1)
+    })
+    test('should add item in order 1 , 2', () => {
+        const queue = []
+        const item1 = { id: '1', initative: 1 }
+        const item2 = { id: '2', initative: 2 }
+        insertInitativeSort(queue, item1)
+        insertInitativeSort(queue, item2)
+        expect(queue.length).toBe(2)
+        expect(queue[0].id).toBe('1')
+        expect(queue[1].id).toBe('2')
+    })
+    test('should add item in order 1 , 2 ,3', () => {
+        const queue = []
+        const item1 = { id: '1', initative: 1 }
+        const item2 = { id: '2', initative: 2 }
+        const item3 = { id: '3', initative: 3 }
+        insertInitativeSort(queue, item1)
+        insertInitativeSort(queue, item3)
+        insertInitativeSort(queue, item2)
+        
+        expect(queue.length).toBe(3)
+        expect(queue[0].id).toBe('1')
+        expect(queue[1].id).toBe('2')
+        expect(queue[2].id).toBe('3')
     })
 })
