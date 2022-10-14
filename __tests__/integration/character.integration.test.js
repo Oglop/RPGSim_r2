7bae7042be43b6a2a5c88229328b1eb86f35f7c3
Oglop <stefgust@gmail.com>
2022-10-14 13:41:20 +0200

    Update character test

--- __tests__/integration/character.integration.test.js
+++ __tests__/integration/character.integration.test.js
@@ -18,7 +18,7 @@ const {
 } = require('../../persistance').queries
 const { executeCommands } = require('../../persistance/commandQueue')
 
-const characterStub = () => {
+const characterMock = () => {
 
     return {
         id: 'A1',
@@ -79,13 +79,13 @@ describe('persistance tests', () => {
     test('character', async () => {
         commands = []
         await migrate()
-        const stub = characterStub()
+        const characterMock = characterMock()
 
-        commands.push({ command: ENUM_COMMANDS.INSERTCHARACTER, data: stub })
-        stub.skills.forEach(s => {
+        commands.push({ command: ENUM_COMMANDS.INSERTCHARACTER, data: characterMock })
+        characterMock.skills.forEach(s => {
             commands.push({ command: ENUM_COMMANDS.INSERTSKILL, data: s })    
         });
-        stub.languages.forEach(l => {
+        characterMock.languages.forEach(l => {
             commands.push({ command: ENUM_COMMANDS.INSERTLANGUAGE, data: l })    
         })
         await executeCommands(commands)
@@ -94,31 +94,31 @@ describe('persistance tests', () => {
         character.skills = await getSkillByCharacterId('A1')
         character.languages = await getLanguageByCharacterId('A1')
         
-        expect(character.name).toBe(stub.name)
-        expect(character.title).toBe(stub.title)
-        expect(character.description).toBe(stub.description)
-        expect(character.gender).toBe(stub.gender)
-        expect(character.birthDate).toStrictEqual(stub.birthDate)
-        expect(character.trait).toBe(stub.trait)
-        expect(character.age).toBe(stub.age)
-        expect(character.health).toBe(stub.health)
-        expect(character.maxHealth).toBe(stub.maxHealth)
-        expect(character.stamina).toBe(stub.stamina)
-        expect(character.maxStamina).toBe(stub.maxStamina)
-        expect(character.religion).toBe(stub.religion)
-        expect(character.coatOfArms).toBe(stub.coatOfArms)
-        expect(character.isAlive).toBe(stub.isAlive)
-        expect(character.stats.str).toBe(stub.stats.str)
-        expect(character.stats.vit).toBe(stub.stats.vit)
-        expect(character.stats.agi).toBe(stub.stats.agi)
-        expect(character.stats.wis).toBe(stub.stats.wis)
-        expect(character.stats.int).toBe(stub.stats.int)
-        expect(character.stats.cha).toBe(stub.stats.cha)
-        expect(character.stats.luc).toBe(stub.stats.luc)
+        expect(character.name).toBe(characterMock.name)
+        expect(character.title).toBe(characterMock.title)
+        expect(character.description).toBe(characterMock.description)
+        expect(character.gender).toBe(characterMock.gender)
+        expect(character.birthDate).toStrictEqual(characterMock.birthDate)
+        expect(character.trait).toBe(characterMock.trait)
+        expect(character.age).toBe(characterMock.age)
+        expect(character.health).toBe(characterMock.health)
+        expect(character.maxHealth).toBe(characterMock.maxHealth)
+        expect(character.stamina).toBe(characterMock.stamina)
+        expect(character.maxStamina).toBe(characterMock.maxStamina)
+        expect(character.religion).toBe(characterMock.religion)
+        expect(character.coatOfArms).toBe(characterMock.coatOfArms)
+        expect(character.isAlive).toBe(characterMock.isAlive)
+        expect(character.stats.str).toBe(characterMock.stats.str)
+        expect(character.stats.vit).toBe(characterMock.stats.vit)
+        expect(character.stats.agi).toBe(characterMock.stats.agi)
+        expect(character.stats.wis).toBe(characterMock.stats.wis)
+        expect(character.stats.int).toBe(characterMock.stats.int)
+        expect(character.stats.cha).toBe(characterMock.stats.cha)
+        expect(character.stats.luc).toBe(characterMock.stats.luc)
         expect(character.languages[0].language).toBe(ENUM_LANGUAGES.common)
         expect(character.languages[0].mastery).toBe(10)
         expect(character.skills.length).toBe(2)
-
+ 
 
         character.stats.str = 99
         await executeCommands([ { command: ENUM_COMMANDS.UPDATECHARACTER, data: character } ])
