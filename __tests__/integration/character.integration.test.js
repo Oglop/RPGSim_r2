b2b72245c815c58d5d7e9261a3f4883cc23e25cd
Oglop <stefgust@gmail.com>
2022-10-15 13:22:48 +0200

    update character integration tests

--- __tests__/integration/character.integration.test.js
+++ __tests__/integration/character.integration.test.js
@@ -3,18 +3,14 @@ const {
     ENUM_RACE_NAMES, 
     ENUM_JOB_NAMES, 
     ENUM_CHARACTER_TRAITS, 
-    ENUM_STAT_NAMES,
     ENUM_GODS, 
     ENUM_COMMANDS,
     ENUM_HEALTH_STATUS,
-    ENUM_LANGUAGES
 } = require('../../generic/enums')
 const { textToDate } = require('../../lib/time')
 const { migrate } = require('../../persistance').infrastructure
 const { 
-    getCharacterById,
-    getSkillByCharacterId,
-    getLanguageByCharacterId
+    getCharacterById
 } = require('../../persistance').queries
 const { executeCommands } = require('../../persistance/commandQueue')
 
@@ -28,18 +24,15 @@ const characterMock = () => {
         title: 'title',
         description: 'this is a description',
         gender: ENUM_GENDER.FEMALE,
-        marriedTo: undefined,
+        marriedTo: 'D4',
         mother: 'B2',
         father: 'C3',
         pregnant: false,
         pregnantTime: 1,
         job: ENUM_JOB_NAMES.cleric,
         race: ENUM_RACE_NAMES.darkElf,
-        languages: [{characterId:'A1', mastery: 10, language: ENUM_LANGUAGES.common}],
-        skills: [
-            {characterId: 'A1', name: 'a1' , statsBase: ENUM_STAT_NAMES.agi, luckTest: true, mastery: 1},
-            {characterId: 'A1', name: 'b1' , statsBase: ENUM_STAT_NAMES.str, luckTest: false, mastery: 10}
-        ],
+        languages: [],
+        skills: [],
         statuses: [ENUM_HEALTH_STATUS.UNCONSCIOUS],
         spells: [],
         birthDate: textToDate('111-2-3'),
@@ -80,22 +73,9 @@ describe('persistance tests', () => {
         const charMock = characterMock()
 
         commands.push({ command: ENUM_COMMANDS.INSERTCHARACTER, data: charMock })
-        charMock.skills.forEach(s => {
-            console.log(JSON.stringify(s))
-            commands.push({ command: ENUM_COMMANDS.INSERTSKILL, data: s })    
-        });
-        charMock.languages.forEach(l => {
-            console.log(JSON.stringify(l))
-            commands.push({ command: ENUM_COMMANDS.INSERTLANGUAGE, data: l })    
-        })
-
         await executeCommands(commands)
 
         const character = await getCharacterById('A1')
-        character.skills = await getSkillByCharacterId('A1')
-        character.languages = await getLanguageByCharacterId('A1')
-        console.log('charMock-------------------------------------',JSON.stringify(charMock))
-        console.log('character-------------------------------------',JSON.stringify(character))
 
         expect(character.name).toBe(charMock.name)
         expect(character.title).toBe(charMock.title)
@@ -118,10 +98,7 @@ describe('persistance tests', () => {
         expect(character.stats.int).toBe(charMock.stats.int)
         expect(character.stats.cha).toBe(charMock.stats.cha)
         expect(character.stats.luc).toBe(charMock.stats.luc)
-        expect(character.languages[0].language).toBe(ENUM_LANGUAGES.common)
-        expect(character.languages[0].mastery).toBe(10)
-        expect(character.skills.length).toBe(2)
- 
+        
         character.stats.str = 99
         await executeCommands([ { command: ENUM_COMMANDS.UPDATECHARACTER, data: character } ])
         const updatedCharacter = await getCharacterById('A1')
@@ -130,6 +107,7 @@ describe('persistance tests', () => {
             { command: ENUM_COMMANDS.DELETECHARACTER, data: 'A1' }
         ])
         const deletedCharacter = await getCharacterById('A1')
-        expect(deletedCharacter).toBeFalsy()
+        expect(deletedCharacter).toStrictEqual({})
+        
     })
 })
