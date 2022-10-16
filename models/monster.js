ebbf5ed3e9966ba47b4a41f65ff3b34a1f961697
Oglop <stefgust@gmail.com>
2022-10-16 16:11:19 +0200

    update monster builder

--- models/monster.js
+++ models/monster.js
@@ -28,23 +28,39 @@ const setStats = (enemy) => {
     if (enemy.name == 'goblin') {
         enemy.stats.str = getRandomNumberInRange(2, 4)
         enemy.stats.agi = getRandomNumberInRange(4, 5)
-        enemy.stats.res = getRandomNumberInRange(1, 2)
+        enemy.stats.vit = getRandomNumberInRange(1, 2)
         enemy.stats.int = getRandomNumberInRange(2, 4)
+        enemy.stats.luc = getRandomNumberInRange(2, 8)
         enemy.health = getRandomNumberInRange(4, 6)
     } else if (enemy.name == 'orc') {
         enemy.stats.str = getRandomNumberInRange(5, 7)
         enemy.stats.agi = getRandomNumberInRange(3, 4)
-        enemy.stats.res = getRandomNumberInRange(3, 4)
+        enemy.stats.vit = getRandomNumberInRange(3, 4)
         enemy.stats.int = getRandomNumberInRange(2, 3)
+        enemy.stats.luc = getRandomNumberInRange(1, 2)
         enemy.health = getRandomNumberInRange(7, 9)
     } else if (enemy.name == 'troll') {
         enemy.stats.str = getRandomNumberInRange(9, 13)
         enemy.stats.agi = getRandomNumberInRange(1, 3)
-        enemy.stats.res = getRandomNumberInRange(4, 6)
+        enemy.stats.vit = getRandomNumberInRange(4, 6)
         enemy.stats.int = getRandomNumberInRange(1, 2)
+        enemy.stats.luc = getRandomNumberInRange(1, 2)
         enemy.health = getRandomNumberInRange(16, 21)
     }
 }
+
+/**
+ * 
+ * @param {{id: string, stats: { str: Number, agi: Number, vit: Number, int: Number, luc: Number }}} enemy 
+ */
+const modifyStats = enemy => {
+    enemy.stats.str += getRandomNumberInRange(1,5) - 3
+    enemy.stats.agi += getRandomNumberInRange(1,5) - 3
+    enemy.stats.vit += getRandomNumberInRange(1,5) - 3
+    enemy.stats.int += getRandomNumberInRange(1,5) - 3
+    enemy.stats.luc += getRandomNumberInRange(1,5) - 3
+}
+
 
 /**
  * push attack objects to enemy object
@@ -66,7 +82,7 @@ const getEffectScaling = (enemy, action) => {
     switch (action.statBase) {
         case ENUM_STAT_NAMES.str: stat = enemy.str; break;
         case ENUM_STAT_NAMES.agi: stat = enemy.agi; break;
-        case ENUM_STAT_NAMES.res: stat = enemy.res; break;
+        case ENUM_STAT_NAMES.vit: stat = enemy.vit; break;
         case ENUM_STAT_NAMES.int: stat = enemy.int; break;
     }
     if (stat < 8) { return 1.0 }
@@ -106,5 +122,6 @@ module.exports = {
     setAttacks,
     attack,
     getEffectScaling,
-    attackEffect
+    attackEffect,
+    modifyStats
 }
